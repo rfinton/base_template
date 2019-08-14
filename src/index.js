1f5f16e699713bdd9090d86ce0db16afe94047fb
Ray Finton <rfinton@outlook.com>
2019-08-14 16:17:41 -0500

    updated js and scss files to be empty

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import {MDCRipple} from '@material/ripple';
 import './main.scss';
-
-const buttonRipple = [].slice.call(document.querySelectorAll('.mdc-button'));
-
-buttonRipple.forEach((el) => {
-  new MDCRipple(el);
-});
